5c4dc1545392059ba4baabacf79b3c9bcac0a5e4
Piyush <jayantsingh0101@gmail.com>
2021-12-11 22:48:29 +0530

    Added DbContext string, connection and fixed Students model namespace

--- ShishyaERP/Models/Students.cs
+++ ShishyaERP/Models/Students.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ShishyaERP
+namespace ShishyaERP.Models
 {
     public class Students
     {
