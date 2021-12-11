3449e5c7114c2b2951cf1bd07acf01b030d7ef48
Piyush <jayantsingh0101@gmail.com>
2021-12-11 22:04:56 +0530

    Created Student property fields model and DbContext

---
+++ ShishyaERP/Students.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShishyaERP
+{
+    public class Students
+    {
+        public int studentId { get; set; }
+        public string studentName { get; set; }
+        public string fatherName { get; set; }
+        public string motherName { get; set; }
+    }
+}
